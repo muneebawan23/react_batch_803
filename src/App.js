4d51a71e4ab07b30e1e2c0b5afc3e33fcc79844e
muneebawan23 <muneebshehzad67@gmail.com>
2024-01-06 00:43:50 +0500

    changes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import HomePage from "./pages/homePage";
+import Layout from "./global/layout";
 function App() {
   return (
     <div className="App">
       
-      
+      <Layout />
       <HomePage />
       
     </div>
---
+++ src/global/layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+
+const Layout = () => {
+  return (
+    <div>
+        <Header />
+        <Footer/>
+      Layout
+    </div>
+  )
+}
+
+export default Layout
