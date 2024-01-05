cad7c6323da587ecc18acbe2b559993879bb5114
muneebawan23 <muneebshehzad67@gmail.com>
2024-01-05 14:15:24 +0500

    app updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import HomePage from "./pages/homePage";
+function App() {
+  return (
+    <div className="App">
+      
+      
+      <HomePage />
+      
+    </div>
+  );
+}
+
+export default App;
